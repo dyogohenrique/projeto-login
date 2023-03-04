726340f1458fcaebdd4cd4410b3a05f8eb750493
Dyogo Henrique <105476232+dyogohenrique@users.noreply.github.com>
2023-03-04 16:12:55 -0400

    Acesso ao login 1
    Iniciando a página para acessar a "conta"

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,19 @@ const recuperarSenha = document.getElementById('bntEsqSenha');
 recuperarSenha.addEventListener('click' ,() => {
     window.location.href = 'recuSenha.html';
 });
+
+function autenticacao() {
+    let email = document.getElementById('iemail').value;
+    let senha = document.getElementById('isenha').value;
+
+    if (email === 'exemplo@gmail.com' && senha === '1234'){
+            window.location.href = 'acesso.html';
+    } else {
+        window.alert('Email ou senha estão incorretos.');
+    }
+}
+
+document.getElementById('bntEntrar').addEventListener('click', function(event) {
+    event.preventDefault();
+    autenticacao();
+});
